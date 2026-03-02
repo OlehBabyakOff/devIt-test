27fc2f13ff8c915daf6e34195dfd7292c0f2b785
Oleh Babyak <babyakwork@gmail.com>
2026-03-02 18:36:22 +0200

    updated config + fetch service

--- apps/frontend/src/services/fetch-index.ts
+++ apps/frontend/src/services/fetch-index.ts
@@ -3,9 +3,5 @@ import { CONFIGS } from "@configs/index";
 export async function fetchIndex(index: number) {
   const res = await fetch(`${CONFIGS.API_URL}/api?index=${index}`);
 
-  if (!res.ok) {
-    throw new Error(`Status: ${res.status}`);
-  }
-
   return res.json();
 }
