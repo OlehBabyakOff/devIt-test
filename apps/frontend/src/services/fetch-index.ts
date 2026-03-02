df30889f2e1881444dee7523a404a63e1f2379ad
Oleh Babyak <babyakwork@gmail.com>
2026-03-02 16:21:30 +0200

    added fetch index service

---
+++ apps/frontend/src/services/fetch-index.ts
@@ -0,0 +1,11 @@
+import { CONFIGS } from "@configs/index.ts";
+
+export async function fetchIndex(index: number) {
+  const res = await fetch(`${CONFIGS.API_URL}/api?index=${index}`);
+
+  if (!res.ok) {
+    throw new Error(`Status: ${res.status}`);
+  }
+
+  return res.json();
+}
