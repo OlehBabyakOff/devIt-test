d079c84a2dbaf6eb541d28de63bac27fe106cd2b
Oleh Babyak <babyakwork@gmail.com>
2026-03-02 16:19:33 +0200

    added aliases

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@configs": path.resolve(__dirname, "src/configs"),
+    },
+  },
 });
