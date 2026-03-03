f1dd90563808ac8a4307fd90be6d8ad307a22669
Oleh Babyak <babyakwork@gmail.com>
2026-03-03 20:54:10 +0200

    updated app file and request form component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,30 +1,22 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import { RequestForm } from "@components/RequestForm";
 
-import { fetchIndex } from "@services/fetch-index";
+import { useRequest } from "@hooks/useRequest";
 
 function App() {
-  // TODO update response type
-  const [responses, setResponses] = useState<any[]>([]);
+  const { results, successfulResults, run, isRunning } = useRequest();
 
   const lastItemRef = useRef<HTMLLIElement | null>(null);
 
-  // TODO update response type
-  const handleSubmit = async (input: number) => {
-    const res = await fetchIndex(input);
-
-    return res;
+  const handleSubmit = (limit: number) => {
+    run(1000, limit);
   };
 
-  // TODO update response type
-  const handleResponse = (response: any) => {
-    setResponses((prev) => [...prev, response]);
-  };
-
+  // scroll results to the last item
   useEffect(() => {
     lastItemRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [responses]);
+  }, [results]);
 
   return (
     <>
@@ -33,22 +25,29 @@ function App() {
           <h1 className="text-3xl font-bold text-blue-600 text-center mb-5">
             DevIt data fetch
           </h1>
-          <RequestForm onSubmit={handleSubmit} onResponse={handleResponse} />
+          <RequestForm onSubmit={handleSubmit} isRunning={isRunning} />
         </div>
 
         <div className="border-4 border-blue-500 rounded-2xl p-8 flex flex-col items-center space-y-6 shadow-lg bg-white w-full max-w-md">
-          <h1 className="text-3xl font-bold text-blue-600 text-center mb-5">
+          <h1 className="text-3xl font-bold text-blue-600 text-center">
             Responses
           </h1>
+          {successfulResults > 1 && (
+            <p className="text-2xl font-semibold text-blue-600 text-center mb-5">
+              successful: {successfulResults}
+            </p>
+          )}
           <div className="w-full h-96 overflow-y-auto">
             <ul className="space-y-2">
-              {responses.map((res, i) => (
+              {results.map((res, i) => (
                 <li
                   key={i}
-                  ref={i === responses.length - 1 ? lastItemRef : null}
-                  className={`${res.status === "Success" ? "bg-blue-100 text-blue-800" : "bg-red-100 text-red-800"} px-4 py-2 rounded-lg shadow-sm break-words`}
+                  ref={i === results.length - 1 ? lastItemRef : null}
+                  className={
+                    "bg-blue-100 text-blue-800 px-4 py-2 rounded-lg shadow-sm break-words"
+                  }
                 >
-                  {JSON.stringify(res)}
+                  {res}
                 </li>
               ))}
             </ul>
--- apps/frontend/src/components/RequestForm.tsx
+++ apps/frontend/src/components/RequestForm.tsx
@@ -7,51 +7,47 @@ import { Input } from "./core/Input";
 import { requestSchema } from "@schemas/request";
 
 interface RequestFormProps {
-  // TODO update response type
-  onSubmit: (input: number) => Promise<any>;
-
-  // TODO update response type
-  onResponse: (response: any) => void;
+  onSubmit: (limit: number) => void;
+  isRunning: boolean;
 }
 
-export const RequestForm = ({ onSubmit, onResponse }: RequestFormProps) => {
+export const RequestForm = ({ onSubmit, isRunning }: RequestFormProps) => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm<{ input: number }>({ resolver: yupResolver(requestSchema) });
 
   const submitForm = async (data: { input: number }) => {
-    const response = await onSubmit(data.input);
-
-    onResponse(response);
+    onSubmit(data.input);
   };
 
   return (
     <form
       onSubmit={handleSubmit(submitForm)}
-      className="flex items-center space-x-4 w-full max-w-md"
+      className="flex items-start space-x-4 w-full max-w-md"
     >
       <div className="flex-1 flex flex-col">
         <Input
           {...register("input")}
+          disabled={isRunning}
           placeholder="Enter number from 0 to 100"
           className="w-full px-4 py-3 text-lg rounded-lg border-2 border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 transition outline-none"
         />
-        {errors.input && (
-          <p className="text-red-600 text-base font-medium mt-1">
-            {errors.input.message || ""}
-          </p>
-        )}
+
+        <p className="text-red-600 text-base font-medium mt-1 min-h-[24px]">
+          {errors.input?.message || ""}
+        </p>
       </div>
 
       <div className="ml-2 flex items-center h-full">
         <Button
           type="submit"
-          isLoading={isSubmitting}
+          isLoading={isRunning}
+          disabled={isRunning}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold text-lg px-6 py-3 rounded-lg transition-colors shadow-md"
         >
-          Send request
+          Start
         </Button>
       </div>
     </form>
