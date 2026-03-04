21289685f5a278a952d03035859e70b4ce1dfdc4
Oleh Babyak <babyakwork@gmail.com>
2026-03-04 22:44:21 +0200

    added rateLimit middleware to the router

--- apps/backend/src/routes/api.route.ts
+++ apps/backend/src/routes/api.route.ts
@@ -2,7 +2,11 @@ import { Router } from 'express';
 
 import { apiController } from '../controllers/api.controller.js';
 
+import { rateLimit } from '../middlewares/rateLimit.middleware.js';
+
 const router = Router();
+
+router.use(rateLimit(50, 1000));
 
 router.get('/api', apiController.getIndex);
 
