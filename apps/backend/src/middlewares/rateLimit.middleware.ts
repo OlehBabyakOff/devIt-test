f35aeeb7e4ac28f2d2e12a3632554b5f0812f993
Oleh Babyak <babyakwork@gmail.com>
2026-03-04 22:44:08 +0200

    created rateLimit middleware

---
+++ apps/backend/src/middlewares/rateLimit.middleware.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from 'express';
+import type { RedisWithScripts } from '../infrastructure/redis/interfaces/redisWithScripts.interface.js';
+
+import { randomUUID } from 'crypto';
+
+import { redisClient } from '../infrastructure/redis/redisClient.js';
+
+export function rateLimit(limit: number, window: number) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const now = Date.now();
+    const key = `rate_limit:global`;
+    const uuid = randomUUID();
+
+    try {
+      const redis = redisClient.getClient() as RedisWithScripts;
+
+      const isAllowed = await redis.rateLimit(key, now, limit, window, uuid);
+
+      if (!isAllowed) {
+        return res.status(429).json({ message: 'Too many requests' });
+      }
+
+      next();
+    } catch (error) {
+      // Todo add logger + handle
+      console.error(error);
+      next();
+    }
+  };
+}
+
