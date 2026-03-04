ddd8af1e220b069f5e3fccb8da6665da19dfb39e
Oleh Babyak <babyakwork@gmail.com>
2026-03-04 13:01:10 +0200

    updated request schema

--- apps/frontend/src/schemas/request.ts
+++ apps/frontend/src/schemas/request.ts
@@ -3,6 +3,8 @@ import * as yup from "yup";
 export const requestSchema = yup.object({
   input: yup
     .number()
+    .integer("Value must be an integer")
+    .positive()
     .typeError("Input value must be a number")
     .required("Input value is required")
     .min(0, "Min value is 0")
